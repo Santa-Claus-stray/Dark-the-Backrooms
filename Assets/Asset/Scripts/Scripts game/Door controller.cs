f1bbe12c064c40fc835e9f5cf28bef05c68050f5
Santa Claus <arturgrigorev9316@gmail.com>
2025-02-14 21:55:50 +0300

    third commit

--- Assets/Asset/Scripts/Scripts game/Door controller.cs
+++ Assets/Asset/Scripts/Scripts game/Door controller.cs
@@ -2,86 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AdvancedDoorController : MonoBehaviour
+public class DoorController : MonoBehaviour
 {
-    public AudioClip openDoorSound; // Звук открытия двери
-    public AudioClip closeDoorSound; // Звук закрытия двери
-    public AudioClip lockedDoorSound; // Звук заблокированной двери
-    public float doorOpenSpeed = 2f; // Скорость открытия двери
-
+    public AudioClip openSound; // Звук открытия двери
+    public AudioClip lockedSound; // Звук заблокированной двери
+    public bool isLocked = true; // Состояние двери (заблокирована или нет)
     private AudioSource audioSource;
-    private Animator animator;
-    private bool isLocked = true; // Дверь изначально заблокирована
-    private bool isOpen = false; // Дверь изначально закрыта
 
-    void Start()
+    private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        animator = GetComponent<Animator>();
+        audioSource = gameObject.AddComponent<AudioSource>();
     }
 
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.E)) // Проверяем, нажата ли клавиша "E"
+        if (other.CompareTag("Player")) // Проверяем, что игрок входит в зону триггера
         {
-            TryToggleDoor();
-        }
-    }
-
-    void TryToggleDoor()
-    {
-        if (isLocked)
-        {
-            PlayLockedSound(); // Если дверь заблокирована, воспроизводим звук
-        }
-        else
-        {
-            if (isOpen)
+            if (Input.GetKeyDown(KeyCode.E)) // Проверяем нажатие клавиши E
             {
-                CloseDoor(); // Закрываем дверь
-            }
-            else
-            {
-                OpenDoor(); // Открываем дверь
+                if (isLocked)
+                {
+                    PlayLockedSound();
+                }
+                else
+                {
+                    OpenDoor();
+                }
             }
         }
     }
 
-    void OpenDoor()
+    private void OpenDoor()
     {
-        animator.SetBool("isOpen", true); // Запускаем анимацию открытия
-        audioSource.PlayOneShot(openDoorSound); // Воспроизводим звук открытия
-        isOpen = true;
-        Debug.Log("Дверь открыта!");
+        // Здесь вы можете добавить анимацию открытия двери или логику перемещения
+        transform.Rotate(0, 90, 0); // Пример поворота двери на 90 градусов
+        PlayOpenSound();
     }
 
-    void CloseDoor()
+    private void PlayOpenSound()
     {
-        animator.SetBool("isOpen", false); // Запускаем анимацию закрытия
-        audioSource.PlayOneShot(closeDoorSound); // Воспроизводим звук закрытия
-        isOpen = false;
-        Debug.Log("Дверь закрыта!");
+        audioSource.clip = openSound;
+        audioSource.Play();
     }
 
-    void PlayLockedSound()
+    private void PlayLockedSound()
     {
-        audioSource.PlayOneShot(lockedDoorSound); // Воспроизводим звук заблокированной двери
-        Debug.Log("Дверь заблокирована!");
-    }
-
-    // Метод для разблокировки двери (можно вызывать из другого скрипта)
-    public void UnlockDoor()
-    {
-        isLocked = false;
-        Debug.Log("Дверь разблокирована!");
-    }
-
-    // Метод для блокировки двери (можно вызывать из другого скрипта)
-    public void LockDoor()
-    {
-        isLocked = true;
-        Debug.Log("Дверь заблокирована!");
+        audioSource.clip = lockedSound;
+        audioSource.Play();
     }
 }
+
 
 
